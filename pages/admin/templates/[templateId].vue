aaa8a4742776ade88b3cceea5bed5f93864fe087
JunMin765677 <cheerful.2318@gmail.com>
2025-11-30 22:55:29 +0800

    update-3

--- pages/admin/templates/[templateId].vue
+++ pages/admin/templates/[templateId].vue
@@ -19,7 +19,7 @@
           <input 
             type="search" 
             v-model="searchQuery" 
-            placeholder="搜尋姓名或身分證號..."
+            placeholder="搜尋姓名或身分證號...."
             class="search-input"
           />
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
